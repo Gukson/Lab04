3dbd8b82b7e1e8057b271eb9fbd63fd022ca01e6
Gukson <75785907+Gukson@users.noreply.github.com>
2023-11-15 23:41:05 +0100

    Created Dao interface (#4)
    * Created Dao interface
    * Created dictionary for DataBase
    * Removed comments from MeasurementExternal file

--- lab04_client/src/main/java/io/github/gukson/lab04/client/model/MeasurementExternal.java
+++ lab04_client/src/main/java/io/github/gukson/lab04/client/model/MeasurementExternal.java
@@ -7,34 +7,23 @@ import com.google.gson.annotations.SerializedName;
 
 public class MeasurementExternal {
     @SerializedName("id_stacji")
-    //@Expose
     private String idStacji;
     @SerializedName("stacja")
-    //@Expose
     private String stacja;
     @SerializedName("data_pomiaru")
-    //@Expose
     private String dataPomiaru;
     @SerializedName("godzina_pomiaru")
-    //@Expose
     private String godzinaPomiaru;
-    //@SerializedName("temperatura")
-    //@Expose
     private String temperatura;
     @SerializedName("predkosc_wiatru")
-    //@Expose
     private String predkoscWiatru;
     @SerializedName("kierunek_wiatru")
-    //@Expose
     private String kierunekWiatru;
     @SerializedName("wilgotnosc_wzgledna")
-    //@Expose
     private String wilgotnoscWzgledna;
     @SerializedName("suma_opadu")
-    //@Expose
     private String sumaOpadu;
     @SerializedName("cisnienie")
-    //@Expose
     private String cisnienie;
 
     public String getIdStacji() {
