46aa05947cb2264226fb04fdfe5886c9539260d2
Gukson <75785907+Gukson@users.noreply.github.com>
2023-11-16 14:23:32 +0100

    Updated MeasurementService file
    * Updated MeasurementService file
    * Updated ImportNewData method
    * Created checkDataStatus method

--- lab04_client/src/main/java/io/github/gukson/lab04/client/model/MeasurementService.java
+++ lab04_client/src/main/java/io/github/gukson/lab04/client/model/MeasurementService.java
@@ -6,6 +6,8 @@ import io.github.gukson.lab04.client.dao.MeasurementDao;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class MeasurementService {
     private URL url;
@@ -16,13 +18,29 @@ public class MeasurementService {
         this.mDao = mDao;
     }
 
-    public void ImportNewData(String tableName){
-        try {
-            MeasurmentExternal[] data = getJsonFromAPI();
-//            for(MeasurmentExternal m: data){
-//
-//            }
+    public void checkDataStatus(LocalDate todaysDate, String tableName){
+        if(!mDao.areDataFromThisDay(todaysDate)){
+            this.ImportNewData(tableName);
+        }
+    }
 
+    private void ImportNewData(String tableName){
+        try {
+            MeasurmentExternal[] data = this.getJsonFromAPI();
+            for(MeasurmentExternal m: data){
+                mDao.save(new Measurement(
+                        Integer.parseInt(m.getIdStacji()),
+                        m.getStacja(),
+                        LocalDate.parse(m.getDataPomiaru()),
+                        LocalTime.parse(m.getGodzinaPomiaru()),
+                        Double.valueOf(m.getTemperatura()),
+                        Integer.parseInt(m.getPredkoscWiatru()),
+                        Integer.parseInt(m.getKierunekWiatru()),
+                        Double.valueOf(m.getWilgotnoscWzgledna()),
+                        Double.valueOf(m.getSumaOpadu()),
+                        Double.valueOf(m.getCisnienie())
+                ));
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -34,5 +52,4 @@ public class MeasurementService {
         MeasurmentExternal[] data =  gson.fromJson(reader, MeasurmentExternal[].class);
         return data;
     }
-
 }
