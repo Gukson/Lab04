0721c61ade2e05b387e038f413d021e3b6355e94
Gukson <75785907+Gukson@users.noreply.github.com>
2023-11-16 21:48:45 +0100

    Created DataBase setup
    * Created dataBase files
    * Created connection to DB
    * Created View class
    * Deleted .gitkeep files
    * Changes localization of MeasurementService.java file
    * Used apache commons to null safely parse
    * Created extra constructor for Measurement

--- lab04_client/src/main/java/io/github/gukson/lab04/client/service/MeasurementService.java
+++ lab04_client/src/main/java/io/github/gukson/lab04/client/service/MeasurementService.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.time.LocalDate;
-import java.time.LocalTime;
+
+import static org.apache.commons.lang3.math.NumberUtils.toDouble;
+import static org.apache.commons.lang3.math.NumberUtils.toInt;
 
 public class MeasurementService {
     private URL url;
--- lab04_client/src/main/java/module-info.java
+++ lab04_client/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module io.github.gukson.lab04.client{
     requires com.google.gson;
     requires javax.annotation.api;
     requires java.sql;
+    requires org.apache.commons.lang3;
     opens io.github.gukson.lab04.client.model to com.google.gson;
     opens io.github.gukson.lab04.client.service to com.google.gson;
 }
