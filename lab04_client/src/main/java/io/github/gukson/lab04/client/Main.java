108092621e9fa0d5c60779792762d5fa3143ff8c
Gukson <75785907+Gukson@users.noreply.github.com>
2023-11-16 02:23:32 +0100

    Repaired Files names again (#7)

--- lab04_client/src/main/java/io/github/gukson/lab04/client/Main.java
+++ lab04_client/src/main/java/io/github/gukson/lab04/client/Main.java
@@ -2,7 +2,7 @@ package io.github.gukson.lab04.client;
 
 import com.google.gson.Gson;
 
-import io.github.gukson.lab04.client.model.MeasurementExternal;
+import io.github.gukson.lab04.client.model.MeasurmentExternal;
 
 
 public class Main {
@@ -10,7 +10,7 @@ public class Main {
         Gson gson = new Gson();
         String json = "[{\"id_stacji\":\"12295\",\"stacja\":\"Bia\\u0142ystok\",\"data_pomiaru\":\"2023-11-15\",\"godzina_pomiaru\":\"16\",\"temperatura\":\"4.2\",\"predkosc_wiatru\":\"3\",\"kierunek_wiatru\":\"310\",\"wilgotnosc_wzgledna\":\"93.1\",\"suma_opadu\":\"12.2\",\"cisnienie\":\"1004.4\"}, {\"id_stacji\":\"12295\",\"stacja\":\"Bia\\u0142ystok\",\"data_pomiaru\":\"2023-11-15\",\"godzina_pomiaru\":\"16\",\"temperatura\":\"4.2\",\"predkosc_wiatru\":\"3\",\"kierunek_wiatru\":\"310\",\"wilgotnosc_wzgledna\":\"93.1\",\"suma_opadu\":\"12.2\",\"cisnienie\":\"1004.4\"}] ";
 
-        MeasurementExternal[] data =  gson.fromJson(json, MeasurementExternal[].class);
+        MeasurmentExternal[] data =  gson.fromJson(json, MeasurmentExternal[].class);
         System.out.println(data[0].getStacja());
         System.out.println(data[0]);
         System.out.println(data[1]);
