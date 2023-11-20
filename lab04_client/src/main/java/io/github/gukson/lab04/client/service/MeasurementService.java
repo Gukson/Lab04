4cd00d40c13966b7c25d9d25f9ba389e1a148b46
Gukson <75785907+Gukson@users.noreply.github.com>
2023-11-20 01:26:28 +0100

    Configured log4j2 (#14)
    * Imported Client from lab04-client to lab04-gui
    * log4j2 WORKS

--- lab04_client/src/main/java/io/github/gukson/lab04/client/service/MeasurementService.java
+++ lab04_client/src/main/java/io/github/gukson/lab04/client/service/MeasurementService.java
@@ -4,16 +4,24 @@ import com.google.gson.Gson;
 import io.github.gukson.lab04.client.dao.MeasurementDao;
 import io.github.gukson.lab04.client.model.Measurement;
 import io.github.gukson.lab04.client.model.MeasurmentExternal;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.time.LocalDate;
 
+
+
 import static org.apache.commons.lang3.math.NumberUtils.toDouble;
 import static org.apache.commons.lang3.math.NumberUtils.toInt;
 
 public class MeasurementService {
+
+    private static final Logger logger = LogManager.getLogger("vehicles");
+
     private URL url;
     private MeasurementDao mDao;
 
@@ -28,7 +36,13 @@ public class MeasurementService {
     public void checkDataStatus(LocalDate todaysDate){
         if(!mDao.areDataFromThisDay(todaysDate)){
             this.importNewData(this.tableName);
+            logger.info("Zaktualizowano dane");
+
         }
+        else {
+            logger.info("Dane są aktualne");
+        }
+
     }
 
     private void importNewData(String tableName){
--- lab04_client/src/main/java/module-info.java
+++ lab04_client/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module io.github.gukson.lab04.client{
     requires javax.annotation.api;
     requires java.sql;
     requires org.apache.commons.lang3;
+    requires org.apache.logging.log4j;
     opens io.github.gukson.lab04.client.model to com.google.gson;
     opens io.github.gukson.lab04.client.service to com.google.gson;
     exports io.github.gukson.lab04.client;
