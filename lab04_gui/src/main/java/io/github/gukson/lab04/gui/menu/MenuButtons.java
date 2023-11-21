59500a818b899248dcc290656eba4805683627fd
Gukson <75785907+Gukson@users.noreply.github.com>
2023-11-21 20:29:20 +0100

    Implemented chars View
    * Implemented charts view without imported data from database

--- lab04_gui/src/main/java/io/github/gukson/lab04/gui/menu/MenuButtons.java
+++ lab04_gui/src/main/java/io/github/gukson/lab04/gui/menu/MenuButtons.java
@@ -1,21 +1,31 @@
 package io.github.gukson.lab04.gui.menu;
 
-import io.github.gukson.lab04.client.dao.MeasurementDao;
+import io.github.gukson.lab04.gui.CoreUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 public class MenuButtons {
-    public void buttonsGenerator(List<Integer> data, JPanel rowHolderPanel){
+    public void buttonsGenerator(List<Integer> data, JPanel rowHolderPanel, MenuGUI menuGUI){
         System.out.println(data.size());
         Integer licznik = 0;
         zewn: while(true){
             JPanel panel = new JPanel(new GridLayout(0,4));
             panel.setBackground(new Color(33, 59, 68));
             for(int x = 0; x<4;x++){
-                panel.add(new JButton(String.valueOf(data.get(licznik))));
-                //TODO ActionListener
+                JButton button = new JButton(String.valueOf(data.get(licznik)));
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        CoreUI coreui = (CoreUI) SwingUtilities.getWindowAncestor(menuGUI.getContentPane());
+                        coreui.toogleCharts();
+                    }
+                });
+                panel.add(button);
+
                 licznik++;
                 if(licznik == data.size()){
                     rowHolderPanel.add(panel);
--- lab04_gui/src/main/java/io/github/gukson/lab04/gui/menu/MenuGUI.java
+++ lab04_gui/src/main/java/io/github/gukson/lab04/gui/menu/MenuGUI.java
@@ -46,7 +46,7 @@ public class MenuGUI extends JFrame {
 
         MenuButtons menuButtons = new MenuButtons();
         try{
-            menuButtons.buttonsGenerator(measurementDao.getAllStationId(), rowHolderPanel);
+            menuButtons.buttonsGenerator(measurementDao.getAllStationId(), rowHolderPanel, MenuGUI.this);
         }
         catch (SQLException ignored){}
 
