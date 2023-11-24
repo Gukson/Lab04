a7417f134b5eefd93568d3be9ef2f75b215d1c93
Gukson <75785907+Gukson@users.noreply.github.com>
2023-11-25 00:25:48 +0100

    Changed numbers into names of city in Menu

--- lab04_gui/src/main/java/io/github/gukson/lab04/gui/menu/MenuButtons.java
+++ lab04_gui/src/main/java/io/github/gukson/lab04/gui/menu/MenuButtons.java
@@ -1,5 +1,7 @@
 package io.github.gukson.lab04.gui.menu;
 
+import io.github.gukson.lab04.client.dao.MeasurementDao;
+import io.github.gukson.lab04.client.model.Measurement;
 import io.github.gukson.lab04.gui.CoreUI;
 
 import javax.swing.*;
@@ -7,17 +9,18 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.util.List;
 
 public class MenuButtons {
-    public void buttonsGenerator(List<Integer> data, JPanel rowHolderPanel, MenuGUI menuGUI){
+    public void buttonsGenerator(List<Integer> data, JPanel rowHolderPanel, MenuGUI menuGUI, MeasurementDao measurementDao) throws SQLException {
         Integer buttonsInRow = 5;
         Integer licznik = 0;
         zewn: while(true){
             JPanel panel = new JPanel(new GridLayout(0,buttonsInRow));
             panel.setBackground(new Color(33, 59, 68));
             for(int x = 0; x<buttonsInRow;x++){
-                JButton button = new JButton(String.valueOf(data.get(licznik)));
+                JButton button = new JButton(measurementDao.getStationById(data.get(licznik)));
                 button.setName(String.valueOf(data.get(licznik)));
                 button.setContentAreaFilled(false);
                 button.setCursor(new Cursor(Cursor.HAND_CURSOR));
